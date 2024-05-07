8b523feea36081851246e481478e70e7515680ca
Nicholas Lim <nicknitewolf@gmail.com>
2024-05-07 15:32:18 +0700

    routes: Implement create and delete routes

--- routes.ts
+++ routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import Todo from "./schemas/todo";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
@@ -10,7 +11,7 @@ router.post("/todos", async (req, res) => {
   try {
     const todo = new Todo({ name, isCompleted });
     await todo.save();
-    res.send(todo);
+    res.status(201).send(todo);
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
@@ -22,6 +23,37 @@ router.get("/todos", async (req, res) => {
   try {
     const todos = await Todo.find({});
     res.send(todos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+// Edit a todo
+router.put("/todos/:id", async (req, res) => {
+  const { name, isCompleted } = req.body;
+
+  try {
+    const todo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      {
+        name,
+        isCompleted,
+      },
+      { new: true },
+    );
+    res.status(201).send(todo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
+
+// Delete a todo
+router.delete("/todos/:id", async (req, res) => {
+  try {
+    await Todo.findByIdAndDelete(req.params.id);
+    res.status(204).send({});
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
